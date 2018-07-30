eaf35558379425c00560d18621b4bc011c4f41fa
Daniel Obraczka <daniel@obraczka.de>
2018-07-30 09:58:04 +0200

    Formatted code according to flake8 style guide

--- csvtolatextable.py
+++ csvtolatextable.py
@@ -1,58 +1,78 @@
-import sys, argparse
+import sys
+import argparse
 import pandas as pd
- 
+
+
 precision = 3
 nth = 3
-#colors = ["{\cellcolor{green!20}}","{\cellcolor{orange!20}}","{\cellcolor{yellow!20}}"]
-colors = ["{\cellcolor{blue!20}}","{\cellcolor{green!20}}","{\cellcolor{yellow!20}}"]
+# colors = ["{\cellcolor{green!20}}", "{\cellcolor{orange!20}}",
+# "{\cellcolor{yellow!20}}"]
+colors = ["{\cellcolor{blue!20}}", "{\cellcolor{green!20}}",
+          "{\cellcolor{yellow!20}}"]
 smallest = False
 fulldocument = False
+
 
 def create_header(data_frame):
     rows_string = ''
     if fulldocument:
-        rows_string = r'\documentclass[landscape]{article}' + '\n' + r'\usepackage{xcolor}' + '\n' + r'\usepackage{colortbl}' + '\n' + r'\begin{document}' + '\n'
-        rows_string = rows_string + r'\noindent\makebox[\textwidth]{' +'\n'
-    rows_string = rows_string + r'\begin{tabular}{' + '|c' * data_frame.shape[1] + '|}\n' + '\\hline \n'
-    rows_string = rows_string + " & ".join(data_frame.columns.values.tolist()) + r'\\ ' + '\hline \n'
+        rows_string = r'\documentclass[landscape]{article}' + '\n' +\
+            r'\usepackage{xcolor}' + '\n' + r'\usepackage{colortbl}' + '\n' +\
+            r'\begin{document}' + '\n'
+        rows_string = rows_string + r'\noindent\makebox[\textwidth]{' + '\n'
+    rows_string = rows_string + r'\begin{tabular}{' + '|c' * \
+        data_frame.shape[1] + '|}\n' + '\\hline \n'
+    rows_string = rows_string + " & ".join(data_frame.columns.values.tolist())\
+        + r'\\ ' + '\hline \n'
     return rows_string
 
+
 def get_nth_largest_values(data_frame, dataset, nth, number_of_rows):
-    nlargestValues = pd.to_numeric(data_frame.iloc[dataset][1:]).nlargest(nth).unique()
+    nlargestValues = pd.to_numeric(data_frame.iloc[dataset][1:]).\
+        nlargest(nth).unique()
     count = nth
     while len(nlargestValues) != nth and count <= number_of_rows:
         count = count + 1
-        nlargestValues = pd.to_numeric(data_frame.iloc[dataset][1:]).nlargest(count).unique()
+        nlargestValues = pd.to_numeric(data_frame.iloc[dataset][1:]).\
+            nlargest(count).unique()
     return nlargestValues
 
+
 def get_nth_smallest_values(data_frame, dataset, nth, number_of_rows):
-    nsmallestValues = pd.to_numeric(data_frame.iloc[dataset][1:]).nsmallest(nth).unique()
+    nsmallestValues = pd.to_numeric(data_frame.iloc[dataset][1:]).\
+        nsmallest(nth).unique()
     count = nth
     while len(nsmallestValues) != nth and count <= number_of_rows:
         count = count + 1
-        nsmallestValues = pd.to_numeric(data_frame.iloc[dataset][1:]).nsmallest(count).unique()
+        nsmallestValues = pd.to_numeric(data_frame.iloc[dataset][1:]).\
+            nsmallest(count).unique()
     return nsmallestValues
+
 
 def get_latex_string(data_frame, nth):
     number_of_rows = data_frame.shape[0]
     rows_string = create_header(data_frame)
     for dataset in range(number_of_rows):
         if smallest:
-            nValues = get_nth_smallest_values(data_frame, dataset, nth, number_of_rows)
+            nValues = get_nth_smallest_values(data_frame, dataset, nth,
+                                              number_of_rows)
         else:
-            nValues = get_nth_largest_values(data_frame, dataset, nth, number_of_rows)
+            nValues = get_nth_largest_values(data_frame, dataset, nth,
+                                             number_of_rows)
         for value in range(len(data_frame.iloc[dataset])):
             delimiter_char = r' & '
-            if value == len(data_frame.iloc[dataset]) -1:
+            if value == len(data_frame.iloc[dataset]) - 1:
                 delimiter_char = r'\\' + '\n'
-            if value==0:
-                rows_string = rows_string + data_frame.iloc[dataset][value] + delimiter_char
+            if value == 0:
+                rows_string = rows_string + data_frame.iloc[dataset][value] + \
+                    delimiter_char
             else:
                 cellvalue = data_frame.iloc[dataset][value]
                 extreme_value = False
                 for i in range(len(nValues)):
-                    if cellvalue==nValues[i]:
-                        rows_string = rows_string + colors[i] + str(cellvalue) + delimiter_char
+                    if cellvalue == nValues[i]:
+                        rows_string = rows_string + colors[i] + str(cellvalue)\
+                            + delimiter_char
                         extreme_value = True
                         break
                 if not extreme_value:
@@ -60,22 +80,30 @@ def get_latex_string(data_frame, nth):
     rows_string = rows_string + '\\hline \n \\end{tabular}'
     if fulldocument:
         rows_string = rows_string + '}\n'
-        rows_string = rows_string + '\n' +  r'\end{document}'
+        rows_string = rows_string + '\n' + r'\end{document}'
     return rows_string
+
 
 def main():
     global smallest
     global nth
     global precision
     global fulldocument
-    #Command line arguments
+    # Command line arguments
     parser = argparse.ArgumentParser()
     parser.add_argument("inputpath", help="the path to the .csv table")
-    parser.add_argument("outputpath", help="the path where you want the .tex file")
-    parser.add_argument("-s","--smallest", action="store_true",help="highlight smallest, default is largest")
-    parser.add_argument("-p", "--precision", type=int, help="decimal precision, default is 3")
-    parser.add_argument("-n", "--nelements", type=int, help="highlight largest/smallest, secondlargest/smallest, ..., nthlargest/smallest.\n Must be between 1 and 3")
-    parser.add_argument("-f","--full", action="store_true",help="creates a complete .tex document, rather than only the tabular statement")
+    parser.add_argument("outputpath", help="the path where you want the"
+                        ".tex file")
+    parser.add_argument("-s", "--smallest", action="store_true",
+                        help="highlight smallest, default is largest")
+    parser.add_argument("-p", "--precision", type=int, help="decimal precision"
+                        ",default is 3")
+    parser.add_argument("-n", "--nelements", type=int, help="highlight largest"
+                        "/smallest, secondlargest/smallest, ..., "
+                        "nthlargest/smallest.\n Must be between 1 and 3")
+    parser.add_argument("-f", "--full", action="store_true", help="creates a "
+                        "complete .tex document, rather than only the tabular"
+                        "statement")
     args = parser.parse_args()
     if args.smallest:
         smallest = True
@@ -89,16 +117,17 @@ def main():
     if args.full:
         fulldocument = True
 
-    #Read
-    data_frame = pd.read_csv(args.inputpath,sep='\t')
+    # Read
+    data_frame = pd.read_csv(args.inputpath, sep='\t')
     data_frame = data_frame.round(precision)
 
-    #Get latex string
+    # Get latex string
     result = get_latex_string(data_frame, nth)
 
-    #Write result
+    # Write result
     with open(args.outputpath, 'w') as out_file:
         out_file.write(result)
+
 
 if __name__ == "__main__":
         main()
